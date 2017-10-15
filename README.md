94aa8b1bc5b10d0addc683cdc72f58cdda1eee88
gregoirehj <gregoirehj@gmail.com>
2017-10-15 16:38:43 +0200

    update travis config and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# WebNodeJS" 
+# AST 2017
+
+![Build](https://travis-ci.org/gregoirejh/WebNodeJS.svg?branch=master)
+
+First project for asynchronous server class.
+
+## Installation instructions 
+
+Just clone it from github
+
+## Run 
+
+Use `npm start`
--- test/user.js
+++ test/user.js
@@ -4,36 +4,30 @@ var user = require('../lib/user.js')
 describe('user', function() {
   
   it('saves properly', function(done) {
-    // ....
     user.save("ok", "pwd", function(err) {
       should.not.exist(err)
       done()
     })
   })
-  
+ 
   it('doesn\'t save because missing parameter', function(done) {
-    // ....
     user.save("only name", function(err) {
       should.exist(err)
       done()
     })
   })
   
-  it('get without err', function(done) {
-    // do smthg
+  it('gets properly', function(done) {
     user.get("name", function(err) {
       should.not.exist(err)
       done()
     })
-    //done()
   })
   
-  it('get with err', function(done) {
-    // do smthg
+  it('doesn\'t save because too many parameters', function(done) {
     user.get("name", "pass", function(err) {
       should.exist(err)
       done()
     })
-    //done()
   })
 })
